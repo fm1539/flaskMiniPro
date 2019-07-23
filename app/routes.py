0bbd664d1fd2c379324e7aabbad2fbc15a6b804e
EC2 Default User <ec2-user@ip-172-31-21-7.us-east-2.compute.internal>
2019-07-23 18:55:15 +0000

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,22 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+    
+@app.route('/sentenceResult',methods=['GET','POST'])
+def sentenceResult():
+    userData = dict(request.form)
+    sentenceRoute = userData["sentenceIndex"][0]
+    oddEvenResult = "odd"
+    if model.oddEven(sentenceRoute) == True:
+        oddEvenResult = "even"
+    return render_template('sentenceResult.html',oddEvenResult=oddEvenResult)
+    
+@app.route('/numResult',methods=['GET','POST'])
+def numResult():
+    userData = dict(request.form)
+    num1=int(userData["num1"][0])
+    num2=int(userData["num2"][0])
+    operation=userData["operation"][0].lower()
+    result = model.mathOperation(num1,num2,operation)
+    return render_template('numResult.html',result=result)
